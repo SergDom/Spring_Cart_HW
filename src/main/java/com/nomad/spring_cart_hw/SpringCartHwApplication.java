be9b2443722fed08edac2dd0b081a8b026002f69
sergeydomanov <nomad-labs@yandex.ru>
2022-11-20 13:41:24 +0300

    new

---
+++ src/main/java/com/nomad/spring_cart_hw/SpringCartHwApplication.java
@@ -0,0 +1,13 @@
+package com.nomad.spring_cart_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCartHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCartHwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nomad/spring_cart_hw/SpringCartHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nomad.spring_cart_hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCartHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
